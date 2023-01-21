6138728431b59023694c898471d922df12791627
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-01-21 21:07:33 +0530

    Add files via upload

--- java programs/Addition.java
+++ java programs/Addition.java
@@ -1,11 +1,11 @@
 class Addition{
 public static void main (String [] args) {
 System.out.println("The two number is :");
-int a = 5; // initialize 1st local variable 
-int b = 6; // initialize 2nd local variable  
+int a = 50; // initialize 1st local variable 
+int b = 5; // initialize 2nd local variable  
 System.out.println(a + " " + b); // print line
-int sum = a+b;// logic
+int multi = a*b;// logic
 
-System.out.print("The sum is = " + sum);// print line for addition
+System.out.print("The multi is = " + multi);// print line for addition
 
 }}
---
+++ java programs/Rectangle.java
@@ -0,0 +1,14 @@
+class Rectangle{
+public static void main (String [] args) {
+int l=10;
+System.out.println("the length is - " + l);
+int b=10;
+System.out.println("the breadth is - " + b);
+
+int peremeter=2*(l+b);
+System.out.println("peremeter of rectangle is ="+ peremeter);
+
+int area =l*b;
+System.out.println("area of rectangle is ="+ area);
+}}
+
