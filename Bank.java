bdb0ac50c12ed4a31dc93778668917a42ac01aaa
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-01 21:55:50 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,32 @@
+package mypackage;
+public class Bank {
+	private long account_num;
+	private String name,emial;
+	private float amount;
+	public long getAccount_num() {
+		return account_num;
+	}
+	public void setAccount_num(long account_num) {
+		this.account_num = account_num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmial() {
+		return emial;
+	}
+	public void setEmial(String emial) {
+		this.emial = emial;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	
+
+}
