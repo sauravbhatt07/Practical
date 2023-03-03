b22eef2298392972bbb2d7cecab0385f101e9428
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-03 18:09:52 +0530

    Add files via upload

---
+++ Array/ArrayIndexbound.java
@@ -0,0 +1,10 @@
+package March03;
+
+public class ArrayIndexbound {
+	public static void main(String[] args) {
+		int a[]= {2,5,4,8,9};
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}}}
+		
+	
