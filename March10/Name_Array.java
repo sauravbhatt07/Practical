b3ad5366449a0a2d8439111b0360dfeb426bcd14
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-10 19:55:32 +0530

    Add files via upload

---
+++ March10/Name_Array.java
@@ -0,0 +1,12 @@
+package March10;
+
+public class Name_Array {
+	public static void main(String args[]) {
+		// declaration & initialization of an array
+		double arr[]= {1,2,3};
+		//getting the class name
+		Class a=arr.getClass();
+		String name =a.getName();
+		// print the class name
+		System.out.println(a);
+	}}
