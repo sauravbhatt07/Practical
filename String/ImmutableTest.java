32b684ac83ed8cdaaaf6091212c4f2a982e0edaa
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-13 18:04:48 +0530

    Add files via upload

---
+++ String/ImmutableTest.java
@@ -0,0 +1,10 @@
+package March13String;
+
+public class ImmutableTest {
+	public static void main(String[] args) {
+		String str = "Lucky";
+				str.concat("kr");//insert the string at the end
+				str = str.concat("kr");
+				System.out.println(str);
+	}}
+	
