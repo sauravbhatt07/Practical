a6b18b8244da6bf6d1403f88db9d1cd1b4eba92a
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-28 17:58:01 +0530

    Add files via upload

--- Thread/ThreadExample.java
+++ Thread/ThreadExample.java
@@ -1,14 +1,14 @@
-package March27Thread;
+package March28;
 // creating thread by extending thread class
-class ThreadExample extends Thread {
-public void run() {
-	System.out.println("I am thread .....thread is runninng");
-}
-public static void main(String[] args) {
-	ThreadExample t = new ThreadExample();
-	t.start(); //move the thread to the active state
-t.setName("GOKU");
-String s = t.getName();
-System.out.println(s);
-}
-}
+public class ThreadExample extends Thread {
+	public void run() {
+		System.out.println("I am Thread...thread is running");
+	}
+	public static void main(String[] args) {
+		ThreadExample t = new ThreadExample();
+		t.start();// move the thread to the active state
+		t.start();		
+	}
+	}
+
+
