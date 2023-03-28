7464b728e2e1946d2cac1bdfebdd00c061847ece
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-28 17:55:54 +0530

    Add files via upload

---
+++ Thread/JoinMethod.java
@@ -0,0 +1,25 @@
+package March28;
+
+public class JoinMethod extends Thread{
+	public void run() {
+		for (int i=1;i<=3;i++) {
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e ) {System.out.println(e);
+			}
+			System.out.println(i);
+			}}
+	public static void main(String[] args) {
+		JoinMethod t = new JoinMethod();
+		JoinMethod t1 = new JoinMethod();
+		JoinMethod t2 = new JoinMethod();
+		try {
+			t.join();
+		}
+		catch (Exception e) {
+			System.out.println(e);}
+	t1.start();
+	t2.start();
+		}}
+	
