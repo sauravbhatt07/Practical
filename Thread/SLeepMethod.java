fab06ff988cb4de9a68a184aa3fec04af57e0989
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-03-28 17:57:45 +0530

    Add files via upload

---
+++ Thread/SLeepMethod.java
@@ -0,0 +1,12 @@
+package March28;
+public class SLeepMethod extends Thread {
+	public static void main(String[] args) {
+		try {
+			for(int i=0; i<=5; i++) {
+					Thread.sleep(800);
+					System.out.println(i);
+		}
+	}catch(InterruptedException e) {
+		System.out.println(e);
+	}}}
+
