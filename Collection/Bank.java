6d6e4e0c7c051d24aab9125d5cd3175d7a1b88d5
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-04-07 18:11:59 +0530

    Add files via upload

---
+++ Collection/Bank.java
@@ -0,0 +1,16 @@
+package April07Collection;
+
+class Bank {
+long AccountNo;
+String CustomerName;
+double Balance;
+int CustomerAge;
+public Bank(long AccountNo,String CustomerName,double Balance,int CustomerAge) {
+this.AccountNo = AccountNo;
+this.CustomerName = CustomerName;
+this.Balance = Balance;
+this.CustomerAge = CustomerAge;
+}
+public Bank() {
+	// TODO Auto-generated constructor stub
+}}
