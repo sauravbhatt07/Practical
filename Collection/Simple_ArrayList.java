bf37c3fa8c313e9b30806e6603c9f30dd0ed487a
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-04-07 18:12:14 +0530

    Add files via upload

---
+++ Collection/Simple_ArrayList.java
@@ -0,0 +1,15 @@
+package April07Collection;
+import java.util.ArrayList;
+public class Simple_ArrayList {
+public static void main(String[] args) {
+ArrayList<String> al = new ArrayList<String>(); //creating arraylist
+//add element in arraylist thorugh object
+al.add("Paul");
+al.add("Goku");
+al.add("Ben");
+al.add("Zen");
+al.add("Tj");
+//print  arraylist
+System.out.println(al);
+}
+}
