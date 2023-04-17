563de166663908123a6f3a1e7604dec4356e5e54
sauravbhatt07 <122784756+sauravbhatt07@users.noreply.github.com>
2023-04-17 18:37:03 +0530

    Add files via upload

---
+++ Collection/HashMapEg2.java
@@ -0,0 +1,33 @@
+package April17Mapin;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEg2 {
+	public static void main(String[] args) {
+		// creating map
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+		map.put(101,  "Mango");
+		map.put(108,  "Orange");
+		map.put(105,  "Banana");
+		map.put(103,  "null");
+		map.put(null,  null);
+		map.put(102,  null);
+		map.put(null,  "Apple");
+		map.put(109,  "Litchi");
+		System.out.println(map);
+		// traversing
+		for (Map.Entry m : map.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+		System.out.println("-----------------------------------------------------");
+		map.putIfAbsent(102, "CustardApple");
+		map.putIfAbsent(101, "Watermelon");
+		for (Map.Entry m : map.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
+		map.remove(103);
+		System.out.println(map);
+		
+	}
+}
